23d299b3dda0f145ba2da913bd7e53301eb04078
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-04 15:52:43 +0530

    Create extra_features.py

---
+++ python-notes/extra_features.py
@@ -0,0 +1,23 @@
+# list comprehension
+"""
+    Advantages of List Comprehension
+    * More time-efficient and space-efficient than loops.
+    * Require fewer lines of code.
+    * Transforms iterative statement into a formula.
+"""
+li = [i for i in range(6) if i%2==0]
+print(li)
+
+
+# pop in lists
+li = [1,2,3,4]
+print(li.pop()) # pops last item in list
+print(li.pop(0)) # pop 0th item in list 
+print(li.pop(1)) # pops 1st item in list
+
+# removing key value pair from dictionary
+di = {'1':1, '2':2, 'a':'hi'}
+di.__delitem__('a')  # removes key value pair with key = 'a' from di
+print(di)
+
+

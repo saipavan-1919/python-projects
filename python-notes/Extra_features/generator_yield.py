cc5e892805b368871babc3564d0d5a804f3b0aee
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-07 11:16:53 +0530

    Create generator_yield.py

---
+++ python-notes/Extra_features/generator_yield.py
@@ -0,0 +1,29 @@
+"""
+    Generator:
+        Generator is a function which uses yield keyword instead of return.
+        * if the body of the function consist of yield then that function is known as generator.
+        Yield:
+            yield keyword used to make a function as a generator.
+            yield returns a generator object.
+            A generator object can be iterated.
+        * we can use iteration or next method to iterate through a generator function/object.
+        * 
+"""
+# generator function
+def gen1():
+    for i in range(5):
+        yield i
+
+# creating a generator object
+a = gen1()
+
+# iterating through generator using next method
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+
+# iterating through genrator using for loop
+for i in gen1():
+    print(i)

0dcc2b98bd95e64b2de5525f1285530d134a15c7
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-07 10:45:39 +0530

    Create lambda.py

---
+++ python-notes/Extra_features/lambda.py
@@ -0,0 +1,14 @@
+""" 
+    lambda function
+    Lambda function is known as anonymous function that doesn't have a name.
+    Lambda function contains multiple arguments and single expression
+    syntax : lambda <arguments> : <arguments> : <expression>
+    * Only one expression
+    * performs expression and returns the value
+"""
+
+add = lambda x,y: x+y
+print(add(2,3))
+
+div = lambda x,y: x/y
+print(div(6,3))

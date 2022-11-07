1e350957fd75750975880835c0e1ad780177e10d
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-07 11:38:19 +0530

    Create modules_packages_libraries.py

---
+++ python-notes/Extra_features/modules_packages_libraries.py
@@ -0,0 +1,16 @@
+"""
+    Module:
+        * Python Module is a python file consisting of variables, functions and classes.
+        * Python Module is a .py extension file
+        * ex: datetime, regex, random, etc..
+    Package:
+        * Python Package is a directory which is collection of python modules.
+        * Python Package is a directory with .py files.
+        * ex: Numpy, pandas, etc..
+    Library:
+        * Python Library is a collection of Python Packages.
+        * The Python Library consists of collection of functionalities of codes that allows us to
+          perform many tasks without writing code again.
+        * Python Library is a reusable chunk of code that we can use simply by importing it.
+        * ex: Matplotlib, seaborn, pygame, etc..
+"""

9db28d344f45060a6fafe68cbdd5b9ca33a63c40
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-07 10:46:44 +0530

    Create octal_hexa_ascii_values.py

---
+++ python-notes/Extra_features/octal_hexa_ascii_values.py
@@ -0,0 +1,11 @@
+# ord retunrs the ascii value of a character
+print(ord("a"))
+
+# hex converts integer to hexadecimal number
+print(hex(17))
+
+# oct converts integers to octal numbers
+print(oct(8))
+
+# complex converts numbers into complex numbers
+print(complex(1,2))

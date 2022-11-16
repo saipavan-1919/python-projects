7f58b894e133ceb2e6ce569b50b65758423d0478
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-16 12:22:47 +0530

    Update 6-Selection_sort.py

--- python-notes/problems/6-Selection_sort.py
+++ python-notes/problems/6-Selection_sort.py
@@ -2,14 +2,26 @@
 """
     In selection sort in every pass we find the least element in the array and we will place
     them from the starting of the array.
-
+    * We divide the array into two parts
+        1) sorted array
+        2) unsorted array
+    
     if n elements are presant in a array then we require n-1 passes to sort.
 
-    array =  [5, 3, 1, 2, 4]
-    pass1 -> [1, 3, 5, 2, 4]
-    pass2 -> [1, 2, 5, 3, 4]
-    pass3 -> [1, 2, 3, 5, 4]
-    pass4 -> [1, 2, 3, 4, 5]
+    array =  [5, 3, 1, 2, 4]     -> complete unsorted array
+    pass1 -> [1, 3, 5, 2, 4]     -> sorted array = [1]  unsorted array = [3,5,2,4]
+    pass2 -> [1, 2, 5, 3, 4]     -> sorted array = [1,2]  unsorted array = [5,3,4]
+    pass3 -> [1, 2, 3, 5, 4]     -> sorted array = [1,2,3]  unsorted array = [5,4]
+    pass4 -> [1, 2, 3, 4, 5]     -> sorted array = [1,2,3,4,5]  unsorted array = []
+
+    procedure:
+        * finding the index of lowest element in array
+        * swapping the lowest element with the element which is first in unsorted array.
+
+
+        
+
+
 """
 
 a = [5,3,1,2,4]

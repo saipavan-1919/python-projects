9df710140a63c6063c4d99923e3778d9b383760c
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-16 11:17:52 +0530

    Create string_repetition.py

---
+++ python-notes/Extra_features/string_repetition.py
@@ -0,0 +1,10 @@
+# String repetition
+# we can repeat a string using the '*'(multiplication) symbol
+# str1 = "hi" * 2  ==> str2 = "hihi"
+
+str1 = "hi"*2
+print(str1)
+
+# printing a triangle
+for i in range(1,6):
+    print('*'*i)

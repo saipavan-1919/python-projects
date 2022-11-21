8db935f436cbbb000bfae148ddad8767e812ce61
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-21 11:19:27 +0530

    Update 10-text_starts_with_string.py

--- RegEx/10-text_starts_with_string.py
+++ RegEx/10-text_starts_with_string.py
@@ -3,8 +3,10 @@
 import re
 
 def beg_string(s_str,txt):
-    print(re.findall(f"^{s_str}",txt))
-    if re.search(f"^{s_str}",txt):
+    # \A - used to match if text starts with string
+    # ^ - also used to match if text starts with string 
+    print(re.findall(f"\A{s_str}",txt))  # method 1
+    if re.search(f"^{s_str}",txt):       # method 2
         print("text starts with the string")
     else:
         print("text doesn't starts with string")

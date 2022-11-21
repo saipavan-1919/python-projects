d40beb08ef9c213f442720d1325550b27f913d21
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-21 14:26:24 +0530

    Create topics.py

---
+++ python-notes/topics.py
@@ -0,0 +1,37 @@
+# python topics
+
+"""
+    1) what is python
+    2) python interpreter
+    3) basic data types (int, flaot, bool, complex, float)
+    4) advanced data types (list, tuple, dictionary, set, strings)
+    5) zero indentation and comments
+    6) dynamic typing
+    7) operators
+    8) conditinol statements
+    9) functions 
+    10) indentation
+    11) classes
+    12) object
+    13) inheritance (single, multiple, multilevel, heirachical)
+    14) polymorphism
+    15) abstraction
+    16) encapsulation
+    17) exception handling
+    18) file handling
+    19) pass, continue, break
+    20) namespaces (local, global)
+    21) access specifiers (global, private, protected)
+    22) self
+    23) __init__
+    24) __main__
+    25) generators
+    26) yield
+    27) decorators
+    28) slicing
+    29) docstring
+    30) map function
+    31) type casting
+    32) module, package, library
+
+"""

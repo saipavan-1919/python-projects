b4c748d0de4a68428786d58a59d3be821814808f
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-23 12:26:44 +0530

    Create help_dir_functions.py

---
+++ python-notes/Extra_features/help_dir_functions.py
@@ -0,0 +1,12 @@
+
+# help function
+# it gives the information about the object, functions, methods
+help(list)
+help(str)
+
+
+# dir function
+# this gives the available methods, variables for a object
+# returs list of methods available for objects
+print(dir(list))
+print(dir(str))

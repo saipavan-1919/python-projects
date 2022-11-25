65307df9af8f207d50924f48546e61fc4d439f0b
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-11-25 16:52:22 +0530

    Create evrything_object.py

---
+++ python-notes/evrything_object.py
@@ -0,0 +1,55 @@
+"""
+    In pyhton everything is a object.
+    pyhton is a object oriented programming language.
+    As in pyhton everything is a object the datatypes are also implemented as calsses internally.
+    When we decalre a variable and assign a value during the execution the python will recognise it
+        and keeps it in the required class.
+    If it is 
+            integer type data then that data will be kept in int class.
+            floating number type data then it will be kept in float class.
+            complex number type data then it will be kept in complex class.
+            boolean type data then it will be kept in bool class.
+            string type data then it will be kept in str class.
+
+    * Built in data types/classes are implimented in a differnt way in pyhton not like how we define classes.
+    * We dont know how the datatypes/ classes are implemented in pyhton
+    * But when we refer to the variable it will give the data.
+    * We know that every class consist of two things 1) data 2) object
+    * When we are creating a variable the value we give is data but pyhton will interpret it and creates it as a onject in pyhton.
+
+    * In python data types are treated as classes because these classes will tell how to interpret the 
+        data in memory.
+    * In Python every value has a data type, since everything is an object in Python, data types are actually classes, variables being their objects.
+
+    * In python data types are different their classes are different
+            Datatype            class
+            integers            int
+            floating numbers    float
+            complex numbers     complex
+            strings             str
+            lists               list
+            tuples              typle
+            sets                set
+
+    * the data of datatype is stored in the memory and they can be interpreted correctly using their classes.
+        a = 1
+        here 1 is data of integers type
+             a is object of int class
+        b = 12.22
+        here 12.22 is data of type floating numbers
+             b is object of float class
+        str1 = "hi how are ypu"
+        here "hi how are ypu" is data of string data type 
+             str1 is object of str class
+             strings data can be interpreted by the str class
+        
+
+                __________
+                |        |
+                | object | -------> a  
+                |        |
+                |________|
+
+         when we access a it will give the data in the object.
+         class will interpret the data in the object.
+"""

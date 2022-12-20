4693f6b9b2c376545200ec2183d2d47e2db62529
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-12-20 10:38:38 +0530

    Update operators.py

--- python-notes/operators.py
+++ python-notes/operators.py
@@ -84,4 +84,26 @@
         ~ 	        NOT	                    Inverts all the bits
         <<	        Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off
         >>	        Signed right shift	    Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+
+
+Operators precedence:
+
+Level   Category        Operators
+
+7(high) exponent        **
+
+6       multiplication  *,/,//,%
+
+5       addition        +,-
+
+4       relational      ==,!=,<=,>=,>,<
+
+3       logical         not
+
+2       logical         and
+
+1(low)  logical         or
+
+
 """
+

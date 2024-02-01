1b91a800397f75350f7b00655fb885abb716499f
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2024-02-01 20:44:29 +0530

    Update and rename file_operations.py to 10. file_operations.py

--- python-notes/10. file_operations.py
+++ python-notes/10. file_operations.py
@@ -8,7 +8,8 @@
                 returns a file object
             modes:
                 r: open an existing file for a read operation.
-                w: open an existing file for a write operation. If the file already contains some data then it will be overridden but if the file is not present then it creates the file as well.
+                w: open an existing file for a write operation. If the file already contains some data then
+                   it will be overridden but if the file is not present then it creates the file as well.
                 a:  open an existing file for append operation. It won’t override existing data.
                 r+:  To read and write data into the file. The previous data in the file will be overridden.
                 w+: To write and read data. It will override existing data.
